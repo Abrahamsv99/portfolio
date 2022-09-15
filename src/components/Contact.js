57b0ffb8c1bba2dee647c9111be082ade7b144f1
Abrahamsv99 <104535591+Abrahamsv99@users.noreply.github.com>
2022-09-15 08:36:45 -0500

    Fixed console errors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,15 @@ function Contact() {
     return (
         <div className="container mx-auto my-40 px-12">
             <h1 className="text-4xl py-14">Contact and Information</h1>
-            <div class="grid grid-cols-2 gap-6">
+            <div className="grid grid-cols-2 gap-6">
                 <div>
-                    <ul class="list-disc list-inside marker:text-secondary-focus hover:marker:text-secondary-focus">
+                    <ul className="list-disc list-inside marker:text-secondary-focus hover:marker:text-secondary-focus">
                         <li className="mb-4 hover:underline hover:decoration-secondary underline-offset-8"><a href="https://www.linkedin.com/in/absanchez09/">LinkedIn</a></li>
                         <li className="mb-4 hover:underline hover:decoration-secondary underline-offset-8"><a href="https://github.com/Abrahamsv99">Github</a></li>
                     </ul>
                 </div>
                 <div>
-                    <ul class="list-disc list-inside marker:text-secondary">
+                    <ul className="list-disc list-inside marker:text-secondary">
                         <li className="mb-4 hover:underline hover:decoration-secondary underline-offset-8"><a href="mailto:abrahamsv99@gmail.com">Email: abrahamsv99@gmail.com</a></li>
                         <li className="mb-4 hover:underline hover:decoration-secondary underline-offset-8"><a href={Pdf}>Curriculum vitae</a></li>
                     </ul>
